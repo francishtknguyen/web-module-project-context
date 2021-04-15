f4c9debc2967d34ef8c3519939a56c5b98e44e6a
Francis Nguyen <francishtknguyen@gmail.com>
2021-04-15 15:35:46 -0500

    completed mvp

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 
+import { ProductContext } from './contexts/ProductContext'
+import { CartContext } from './contexts/CartContext'
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
@@ -12,22 +14,41 @@ function App() {
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
+		console.log(item)
+		if(cart.find((i)=> i ===item)) 
+			return
 		// add the given item to the cart
+		setCart([
+			...cart,
+			item
+		])
 	};
 
+	const removeItem = id => {
+		setCart(cart.filter(item => item.id !== id))
+	}
+
+	useEffect(()=> {
+		localStorage.setItem('cart', JSON.stringify(cart))
+	},[cart])
+
 	return (
-		<div className="App">
-			<Navigation cart={cart} />
+		<ProductContext.Provider value={{products, addItem}}>
+		<CartContext.Provider value = {{cart,removeItem}}>
+			<div className="App">
+				<Navigation />
 
-			{/* Routes */}
-			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
-			</Route>
+				{/* Routes */}
+				<Route exact path="/">
+					<Products />
+				</Route>
 
-			<Route path="/cart">
-				<ShoppingCart cart={cart} />
-			</Route>
-		</div>
+				<Route path="/cart">
+					<ShoppingCart />
+				</Route>
+			</div>
+		</CartContext.Provider>
+		</ProductContext.Provider>
 	);
 }
 
